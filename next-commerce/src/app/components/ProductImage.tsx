df52efc5b3d4f391b56ef5f26fced6c5ba150e23
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-28 14:18:10 -0300

    Estilizando componente produto, com loading

---
+++ next-commerce/src/app/components/ProductImage.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState } from "react";
+import Image from "next/image";
+import { ProductType } from "@/types/ProductType";
+
+interface ProductImageProps {
+    product: ProductType;
+    fill?: boolean;
+};
+
+const ProductImage = ({ product, fill }: ProductImageProps) => {
+
+    const [loading, setLoading] = useState<boolean>(true);
+
+    return fill ? (
+        <Image 
+            src={product.image} 
+            fill
+            alt={product.title} 
+            className={`object-cover 
+                ${
+                    loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`
+                }
+            onLoadingComplete={()=>setLoading(false)}
+        />
+    ) : (
+        <Image 
+        src={product.image} 
+        width={400}
+        height={700}
+        alt={product.title} 
+        className={`object-cover 
+            ${
+                loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`
+            }
+        onLoadingComplete={()=>setLoading(false)}
+    />
+    )
+};
+
+export default ProductImage;
--- next-commerce/src/app/page.tsx
+++ next-commerce/src/app/page.tsx
@@ -1,7 +1,13 @@
+import Product from "./components/Product";
 import { ProductType } from "@/types/ProductType";
 
 async function getProducts(){
   const res = await fetch('https://fakestoreapi.com/products');
+
+  if(!res){
+    throw new Error('API ERROR!');
+    return;
+  };
 
   return res.json();
 };
@@ -15,9 +21,7 @@ export default async function Home() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
         {
           products.map((product: ProductType)=>(
-            <div key={product.id}>
-              {product.title}
-            </div>
+            <Product key={product.id} product={product}/>
           ))
         }
       </div>
