3439a56ab5fe478ae3e5c5123f9556a3ed793a26
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-28 07:23:43 -0300

    Adicionando navbar

--- next-commerce/src/app/layout.tsx
+++ next-commerce/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,9 +15,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={inter.className}>
+        <Navbar/>
+        <main className="bg-slate-700 h-screen p-16">
+          {children}
+        </main>
+
+      </body>
     </html>
   );
+
 }
